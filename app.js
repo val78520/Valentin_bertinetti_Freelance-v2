f9f31ac0d5ebba9e0ee1d1723467fe4636da0f41
val78520 <33447164+val78520@users.noreply.github.com>
2019-03-20 14:06:37 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.projets').on("click",function() {
+    alert("Désolé mais je n'ai pas encore finis cette section");
+  })
+})
+
+$(document).ready(function() {
+  $('.passions').on("click",function() {
+    alert("Désolé mais je n'ai pas encore finis cette section");
+  })
+})
