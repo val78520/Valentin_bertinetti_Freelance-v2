01b605bed65f756f7cbb0f5aba908dcea67bf367
val78520 <33447164+val78520@users.noreply.github.com>
2019-03-22 11:27:26 +0100

    JS Maj

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-  $('.projets').on("click",function() {
-    alert("Désolé mais je n'ai pas encore finis cette section");
-  })
-})
-
-$(document).ready(function() {
   $('.passions').on("click",function() {
-    alert("Désolé mais je n'ai pas encore finis cette section");
+    alert("Section indisponible, revenir plus tard.");
   })
 })
